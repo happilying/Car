50956307cfc4bddac94ff1b0f48151e14f77b1fe
micorgames <liujialu021126@163.com>
2024-11-04 11:29:55 +0800

    Add Man!

--- User/inc/IMU.h
+++ User/inc/IMU.h
@@ -1,11 +1,8 @@
 #ifndef _IMU_H
 #define _IMU_H
 
-
 #include "USART.h"
 
-
 void IMU_Init(void);
-
 
 #endif
--- User/src/IMU.c
+++ User/src/IMU.c
@@ -1,7 +1,11 @@
 #include "IMU.h"
 
-
 void IMU_Init(void)
 {
     UART_Init(115200, UART2);
+}
+
+void IMU_Restart(void)
+{
+    
 }
