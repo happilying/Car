c3937b3f926762c2aa18a4045d77e07deb2eeb3b
Tristan Dobos <73593021+TristanDobos@users.noreply.github.com>
2024-11-09 10:37:49 +0100

    Update GPS.h

--- User/inc/GPS.h
+++ User/inc/GPS.h
@@ -3,4 +3,13 @@
 
 #include "USART.h"
 
+#define GPS_UART UART3 // Assuming GPS is connected to UART2
+#define GPS_BUFFER_SIZE 128
+#define NMEA_SENTENCE_MAX_LENGTH 82
+#define GPS_bool u32
+#define GPS_MAX_SIZE 128
+
+void GPS_Init(void);
+void GPS_Get_Location();
+
 #endif
